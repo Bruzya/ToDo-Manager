fc66602a15f31c1d7a4d2526d4a598f2603f8c59
Evgenii Mazrukho <mazrukho@gmail.com>
2023-12-13 11:07:38 +0300

    add files

---
+++ ToDo Manager/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo Manager
+//
+//  Created by Evgenii Mazrukho on 13.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .lightGray
+        title = "Note"
+    }
+
+
+}
+
